8dfb92066041268fadb3be6fbe9625d053cd94f4
bird <zhmzhang@gmail.com>
2017-03-20 16:55:18 +0800

    1、打包管理 2、slf4j 3、工具类

--- ttmonitor/src/main/java/cn/bird/ttmonitor/Server.java
+++ ttmonitor/src/main/java/cn/bird/ttmonitor/Server.java
@@ -1,5 +1,18 @@
 package cn.bird.ttmonitor;
 
-public class Server {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import cn.bird.ttmonitor.util.ConfigUtil;
+
+public class Server {
+	public static final Logger logger = LoggerFactory.getLogger(Server.class);
+	
+	public static void main(String[] args){
+		if(!ConfigUtil.init("server.properties")){
+			logger.error("fail to load configuration");
+			return;
+		}
+		logger.info("server starting...{}", ConfigUtil.config.getString("tt.url"));
+	}
 }
